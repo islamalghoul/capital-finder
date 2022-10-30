f6b3d001afd53ce5c959d0dea5c96219750b551a
islam alghoul <islamalghoul1997>
2022-10-30 20:29:51 +0200

    capital-finder

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,11 +1,16 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    s=self.path
+    print(s)
+
+
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write("hello".encode())
     return
